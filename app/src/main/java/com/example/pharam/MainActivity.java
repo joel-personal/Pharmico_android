5ee8b255bee4a08dc88b4953a2bf2224440a40d3
DelinR <delin@elvicto.in>
2022-11-29 21:36:29 +0530

    first init

---
+++ app/src/main/java/com/example/pharam/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.pharam;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                finish();
+            }
+        }, 1500);
+    }
+}
