ccfb028c83002a6dc7a4ef7408a5b4ed4a0fecac
rDelin <delinraj15@gmail.com>
2022-12-04 17:44:11 +0530

    register

--- app/src/main/java/com/example/pharam/LoginActivity.java
+++ app/src/main/java/com/example/pharam/LoginActivity.java
@@ -2,13 +2,41 @@ package com.example.pharam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    MaterialButton btn_login, btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        handleClickEvents();
+    }
+
+    private void handleClickEvents() {
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
